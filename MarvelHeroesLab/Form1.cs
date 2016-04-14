3add4982c121271a4a77ee618d6404b9741be92e
Nochtal <nochtal@moomoo.com>
2016-04-13 21:08:25 -0700

    Addition of notes and DLL

--- MarvelHeroesLab/Form1.cs
+++ MarvelHeroesLab/Form1.cs
@@ -10,6 +10,17 @@ using System.Windows.Forms;
 
 namespace MarvelHeroesLab
 {
+    ///  Welcome to Marvel Heroes Lab!
+    ///     By Brendan CMG and Nochtal B
+    ///  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ///  For use with the Debugging and Troubleshooting Workshop
+    ///  April 29th, 2016
+    ///  Renton Technical College, hosted by CHIPS
+    ///     3000 NE 4th Street
+    ///     C-102
+    ///     Renton, WA 98056
+    ///   chips@student.rtc.edu
+    
     public partial class Form1 : Form
     {
         public Form1()
